1bcbee69e984583f79018a149280f5a21eeefd9f
fabio.machado <flgmachado@gmail.com>
2018-05-08 15:26:53 -0300

    rascunho de modelo

--- FakeSC/FakeSC/Program.cs
+++ FakeSC/FakeSC/Program.cs
@@ -6,10 +6,48 @@ using System.Threading.Tasks;
 
 namespace FakeSC
 {
+    /* Legenda de variáveis:
+     * largObj -> largura do objeto (plano de distribuição)
+     * compObj -> comprimento do objeto (plano de distribuição)
+     * cadeira [i] -> Cadeira por tipo controlado pelo índice
+     * larg_Item -> Largura da cadeira
+     * comp_Item -> Comprimento da cadeira
+     * iLinha ->
+     * lLinha ->
+     * wLinha ->
+     * comp_usado_faixa ->
+     * y_faixa ->
+     * sobra_comp[j] ->
+     * sobra_larg[k] ->
+     */
     class Program
     {
+        void ordena()
+        {
+            int maior = 0; //identifica qual o maior item para ordenar
+            for (int i = 1; i <= n; i++)
+            {
+                for (iLinha = 1; iLinha <= n; iLinha ++)
+                {
+                    if (comp_Item > maior)
+                    {
+                        maior = w[i];
+                        indice = iLinha;
+                    }
+                }
+                lLinha[i] = l[indice];
+                wLinha[i] = w[indice];
+                z = indice;
+                l[indice] = 0;
+                w[indice] = 0;
+                maior = 0;
+            }
+        }
+
         static void Main(string[] args)
         {
+            
+           
         }
     }
 }
